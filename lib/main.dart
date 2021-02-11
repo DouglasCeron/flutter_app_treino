ad35b68f4440d7a829ebec0fe6b35202274a59d3
DouglasCeron <ceron.douglas@gmail.com>
2021-02-11 16:46:35 -0300

    - adicionando outros Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,42 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('ola AppBar'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'ola body',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-            color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-      onPressed: () => {},
-      backgroundColor: Colors.red[600],
-    ),
-  ),
+  home: Home(),
 
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ola AppBar'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Row(
+        mainAxisAlignment: ,
+        children: [
+          Text('hello'),
+          FlatButton(
+            onPressed: (){},
+            color: Colors.amber,
+            child: Text('click me'),
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text('inside container'),
+
+          )
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () => {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
